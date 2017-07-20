7552484989c2ae61d83a48d553bc1202864deab4
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-07-19 20:33:30 -0700

    persist redux store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,19 @@
 // @flow
 
-import { createStore, applyMiddleware } from 'redux'
+import { AsyncStorage } from 'react-native'
+import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import logger from 'redux-logger'
 import reducer from './redux/reducers'
 
 const middleware = () => (__DEV__ ? applyMiddleware(logger) : null)
 
-const store = createStore(reducer, middleware())
+const store = createStore(
+  reducer,
+  undefined,
+  compose(middleware(), autoRehydrate())
+)
+
+persistStore(store, { storage: AsyncStorage })
 
 export default store
