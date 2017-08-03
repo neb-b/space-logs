37fe8484aed63864f465160977a6eb7a40c2c7ba
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-08-02 22:40:49 -0700

    save dream working

--- src/redux/actions/dream-builder.actions.js
+++ src/redux/actions/dream-builder.actions.js
@@ -10,14 +10,10 @@ export const toggleDreamEditText = createAction(TOGGLE_EDIT_DREAM_TEXT)
 export const updateDreamText = createAction(UPDATE_DREAM_TEXT)
 
 const onSaveDream = createAction(SAVE_DREAM)
-console.log('ONSAVEDREAM', onSaveDream())
 
 export const saveDream = () => {
-  console.log('saving dream')
   return (dispatch, getState) => {
-    console.log('inside????')
-    const state = getState()
-    console.log('state', state)
-    return dispatch(onSaveDream({}))
+    const { dreamText } = getState().dreamBuilder
+    return dispatch(onSaveDream({ dreamText }))
   }
 }
--- src/store.js
+++ src/store.js
@@ -4,9 +4,10 @@ import { AsyncStorage } from 'react-native'
 import { compose, createStore, applyMiddleware } from 'redux'
 import { persistStore, autoRehydrate } from 'redux-persist'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import reducer from './redux/reducers'
 
-const middleware = () => (__DEV__ ? applyMiddleware(logger) : null)
+const middleware = () => (__DEV__ ? applyMiddleware(logger, thunk) : null)
 
 const store = createStore(
   reducer,
